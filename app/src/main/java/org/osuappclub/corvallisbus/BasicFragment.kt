d81c94ba954f671045bbf4f5b949e850375dcefc
Rikki Gibson <rikkigibson@gmail.com>
2015-12-27 18:07:54 -0800

    Create tab bar and add a bunch of icons

---
+++ app/src/main/java/org/osuappclub/corvallisbus/BasicFragment.kt
@@ -0,0 +1,16 @@
+package org.osuappclub.corvallisbus
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by rikkigibson on 12/27/15.
+ */
+class BasicFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_basic, container, false)
+    }
+}
--- app/src/main/java/org/osuappclub/corvallisbus/MainActivity.kt
+++ app/src/main/java/org/osuappclub/corvallisbus/MainActivity.kt
@@ -1,17 +1,88 @@
 package org.osuappclub.corvallisbus
 
+import android.content.Context
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.content.ContextCompat
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.activity_main.*
 
+const val PAGES_COUNT = 4
+
 /**
- * Created by rikkigibson on 12/27/15.
+ * Creates the view pager and tab layout. Container for app fragments which do more interesting things.
  */
 class MainActivity: AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mainText.text = "Let's get started on Corvallis Bus!"
+        pager.adapter = CorvallisBusPagerAdapter(supportFragmentManager, applicationContext)
+        tabLayout.setupWithViewPager(pager)
+
+        // TODO: tab bar icons
+        val drawable = ContextCompat.getDrawable(applicationContext, R.mipmap.ic_launcher)
+        tabLayout.getTabAt(0).setIcon(ContextCompat.getDrawable(applicationContext, R.drawable.favorite))
+        tabLayout.getTabAt(1).setIcon(drawable)
+        tabLayout.getTabAt(2).setIcon(ContextCompat.getDrawable(applicationContext, R.drawable.ic_warning_black_24dp))
+        tabLayout.getTabAt(3).setIcon(ContextCompat.getDrawable(applicationContext, R.drawable.ic_settings_black_24dp))
+
+
+        val supportActionBar = supportActionBar
+        // Set default title text
+        supportActionBar.title = "Favorites"
+
+        pager.addOnPageChangeListener(object: ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+            }
+
+            override fun onPageSelected(position: Int) {
+                supportActionBar.title = when (position) {
+                    0 -> "Favorites"
+                    1 -> "Browse"
+                    2 -> "Service Alerts"
+                    3 -> "Settings"
+                    else -> ""
+                }
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+        })
+    }
+
+    class CorvallisBusPagerAdapter(fm: FragmentManager, context: Context): FragmentPagerAdapter(fm) {
+        val context = context
+        val pages = Array<Fragment?>(4, { null })
+
+        override fun getItem(position: Int): Fragment? {
+            if (position >= PAGES_COUNT) {
+                return null
+            }
+
+            if (pages[position] == null) {
+                pages[position] = createPage(position)
+            }
+            return pages[position]
+        }
+
+        fun createPage(position: Int): Fragment {
+            val bundle = Bundle()
+            bundle.putInt("position", position)
+
+            val fragment = BasicFragment()
+            fragment.arguments = bundle
+
+            return fragment
+        }
+
+        override fun getCount(): Int = PAGES_COUNT
+
     }
 }
